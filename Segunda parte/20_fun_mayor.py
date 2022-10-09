5115d628c49d9750b91288adf5a0b82357949336
mumido <marcoumido@gmail.com>
2022-10-09 13:45:58 -0300

    Solución de ejercicios 20 y 21

---
+++ Segunda parte/20_fun_mayor.py
@@ -0,0 +1,18 @@
+from random import*
+
+
+def genrnd(n):
+    print(n)
+
+n = [randint(-50, 50) for x in range(50)]
+
+print()
+genrnd(n)   
+print()
+
+
+def fun_mayor (num):
+    return (max(n))
+
+print(fun_mayor(n))
+
---
+++ Segunda parte/21_fun_menor.py
@@ -0,0 +1,17 @@
+from random import*
+
+
+def genrnd(n):
+    print(n)
+
+n = [randint(-50, 50) for x in range(50)]
+
+print()
+genrnd(n)   
+print()
+
+
+def fun_menor (num):
+    return (min(n))
+
+print(fun_menor(n))
